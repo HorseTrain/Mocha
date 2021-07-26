465306547ad24b9cbc647a040c571711b74273c7
Raymond <raymond.mataka@gmail.com>
2021-07-26 01:22:54 -0700

    Reality Check.

---
+++ Mocha/Core/GenericCPUInterface.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Core
+{
+	//TODO: Write a power pc frontend using https://github.com/HorseTrain/RecompilerProject
+	//But i need to find a way to effeciently test. Maybe use decaf jit ?
+
+	class GenericCPUContext
+	{
+	public:
+
+
+	};
+}
